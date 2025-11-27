b463897cb411fedb8c80f3ac3214b2f2df4fa5cc
howandt <howandt@gmail.com>
2025-11-27 09:51:23 +0100

    � Fjern midlertidig debugkode fra core.js

--- api/cdf/core.js
+++ api/cdf/core.js
@@ -12,18 +12,6 @@ export default function handler(req, res) {
   }
 
   try {
-    console.log("📁 Debug list of modules dir");
-try {
-  const testDir = path.join(process.cwd(), "api", "cdf", "modules");
-  console.log("👉 Path I'm checking:", testDir);
-  console.log("👉 Exists?", fs.existsSync(testDir));
-  if (fs.existsSync(testDir)) {
-    console.log("👉 Contents:", fs.readdirSync(testDir));
-  }
-} catch (err) {
-  console.error("🛑 Could not read modules dir:", err);
-}
-
     const cwd = process.cwd();
     const basePath = path.join(process.cwd(), "CDF", "modules");
     console.log("📍 process.cwd():", cwd);
