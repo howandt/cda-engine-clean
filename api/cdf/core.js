06c487c596dbefa4d9d0ef0d24cc169e39e5af5f
howandt <howandt@gmail.com>
2025-11-27 09:11:22 +0100

    � Use @vercel/include hints instead of md/json imports

--- api/cdf/core.js
+++ api/cdf/core.js
@@ -1,8 +1,8 @@
 import fs from "fs";
 import path from "path";
-import "./modules/FoodLab.md";
-import "./modules/HealthLab.json";
-import "./modules/recipes_and_health.json";
+// @vercel/include api/cdf/modules/FoodLab.md
+// @vercel/include api/cdf/modules/HealthLab.json
+// @vercel/include api/cdf/modules/recipes_and_health.json
 
 export default function handler(req, res) {
   const { module } = req.query;
