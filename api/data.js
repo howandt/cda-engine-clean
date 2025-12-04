3a29575e77c2b3855eae5b37cf9b97efeab3376b
howandt <howandt@gmail.com>
2025-12-04 12:58:19 +0100

    Begræns til max 5 cases per søgning

--- api/data.js
+++ api/data.js
@@ -32,7 +32,7 @@ export default async function handler(req, res) {
       return res.status(200).json({
         success: true,
         total: 1,
-        source: JSON.stringify(match, null, 2)
+        source: JSON.stringify(filtered.slice(0, 5), null, 2)
       });
     }
 
